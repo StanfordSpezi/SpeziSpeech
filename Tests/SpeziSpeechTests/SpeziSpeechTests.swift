233c9770fff64eaf93693b5117ec95bb16bdb80b
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-11-11 19:39:42 -0800

    Initial Setup

---
+++ Tests/SpeziSpeechTests/SpeziSpeechTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the Stanford Spezi open source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import SpeziSpeech
+import XCTest
+
+
+final class SpeziSpeechTests: XCTestCase {
+    func testSpeziSpeech() throws {
+        XCTAssertTrue(true)
+    }
+}
